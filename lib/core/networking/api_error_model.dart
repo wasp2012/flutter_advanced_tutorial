78e5bc1dce03964007aa0330de1af4fdd513644d
youssefEbeshara <y.sayed@cch-kuwait.com>
2023-12-22 17:53:31 +0200

    22/12
    added api error model
    added base url
    added api error handler

---
+++ lib/core/networking/api_error_model.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'api_error_model.g.dart';
+
+@JsonSerializable()
+class ApiErrorModel {
+  final String? message;
+
+  final int? code;
+  ApiErrorModel({required this.code, required this.message});
+
+  factory ApiErrorModel.fromJson(Map<String, dynamic> json) =>
+      _$ApiErrorModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ApiErrorModelToJson(this);
+}
